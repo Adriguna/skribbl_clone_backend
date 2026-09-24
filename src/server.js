c02d156a9820398b2cce7a116cff438f6f5f2a26
Ritesh Pathak <pathakritesh2004@gmail.com>
2026-09-24 20:04:05 +0530

    Add all files

--- src/server.js
+++ src/server.js
@@ -1652,13 +1652,19 @@ io.on(
 // SERVER
 // ==========================================
 
-const PORT = 5000;
+//const PORT = 5000;
+//
+//server.listen(
+//  PORT,
+//  () => {
+//    console.log(
+//      `Server running on http://localhost:${PORT}`
+//    );
+//  }
+//);
 
-server.listen(
-  PORT,
-  () => {
-    console.log(
-      `Server running on http://localhost:${PORT}`
-    );
-  }
-);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
